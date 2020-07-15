a46a08c106f75eedbbc21b052c6907edb8a07170
atul182000 <atul.18.2000@gmail.com>
2020-07-15 14:53:26 +1200

    changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel2 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
@@ -68,9 +67,10 @@
             this.panel2.Controls.Add(this.loadBullet);
             this.panel2.Controls.Add(this.spinChambers);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(525, 0);
+            this.panel2.Location = new System.Drawing.Point(787, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(369, 531);
+            this.panel2.Size = new System.Drawing.Size(554, 817);
             this.panel2.TabIndex = 1;
             // 
             // button1
@@ -79,11 +79,12 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.SteelBlue;
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Image = global::game2.Resource1.button1_Image;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button1.Location = new System.Drawing.Point(77, 38);
+            this.button1.Location = new System.Drawing.Point(116, 58);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 89);
+            this.button1.Size = new System.Drawing.Size(196, 137);
             this.button1.TabIndex = 3;
             this.button1.Text = "fire Away!!";
             this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -96,9 +97,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.label4.Location = new System.Drawing.Point(32, 280);
+            this.label4.Location = new System.Drawing.Point(48, 431);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(147, 37);
+            this.label4.Size = new System.Drawing.Size(222, 55);
             this.label4.TabIndex = 1;
             this.label4.Text = "Roulette";
             // 
@@ -108,10 +110,11 @@
             this.playAgain.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.playAgain.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.playAgain.ForeColor = System.Drawing.Color.SteelBlue;
-            this.playAgain.Image = ((System.Drawing.Image)(resources.GetObject("playAgain.Image")));
-            this.playAgain.Location = new System.Drawing.Point(228, 395);
+            this.playAgain.Image = global::game2.Resource1.playAgain_Image;
+            this.playAgain.Location = new System.Drawing.Point(342, 608);
+            this.playAgain.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.playAgain.Name = "playAgain";
-            this.playAgain.Size = new System.Drawing.Size(141, 96);
+            this.playAgain.Size = new System.Drawing.Size(212, 148);
             this.playAgain.TabIndex = 4;
             this.playAgain.Text = "Play Again";
             this.playAgain.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -123,9 +126,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.label3.Location = new System.Drawing.Point(31, 222);
+            this.label3.Location = new System.Drawing.Point(46, 342);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(148, 37);
+            this.label3.Size = new System.Drawing.Size(223, 55);
             this.label3.TabIndex = 0;
             this.label3.Text = "Russian ";
             // 
@@ -135,10 +139,11 @@
             this.fireGun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.fireGun.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.fireGun.ForeColor = System.Drawing.Color.SteelBlue;
-            this.fireGun.Image = ((System.Drawing.Image)(resources.GetObject("fireGun.Image")));
-            this.fireGun.Location = new System.Drawing.Point(225, 280);
+            this.fireGun.Image = global::game2.Resource1.fireGun_Image;
+            this.fireGun.Location = new System.Drawing.Point(338, 431);
+            this.fireGun.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.fireGun.Name = "fireGun";
-            this.fireGun.Size = new System.Drawing.Size(141, 92);
+            this.fireGun.Size = new System.Drawing.Size(212, 142);
             this.fireGun.TabIndex = 3;
             this.fireGun.Text = "Fire!!!!";
             this.fireGun.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -151,10 +156,11 @@
             this.loadBullet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.loadBullet.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.loadBullet.ForeColor = System.Drawing.Color.SteelBlue;
-            this.loadBullet.Image = ((System.Drawing.Image)(resources.GetObject("loadBullet.Image")));
-            this.loadBullet.Location = new System.Drawing.Point(228, 38);
+            this.loadBullet.Image = global::game2.Resource1.button1_Image;
+            this.loadBullet.Location = new System.Drawing.Point(342, 58);
+            this.loadBullet.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.loadBullet.Name = "loadBullet";
-            this.loadBullet.Size = new System.Drawing.Size(141, 98);
+            this.loadBullet.Size = new System.Drawing.Size(212, 151);
             this.loadBullet.TabIndex = 1;
             this.loadBullet.Text = "Load Bullet";
             this.loadBullet.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -167,10 +173,11 @@
             this.spinChambers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.spinChambers.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.spinChambers.ForeColor = System.Drawing.Color.SteelBlue;
-            this.spinChambers.Image = ((System.Drawing.Image)(resources.GetObject("spinChambers.Image")));
-            this.spinChambers.Location = new System.Drawing.Point(228, 142);
+            this.spinChambers.Image = global::game2.Resource1.spinChambers_Image;
+            this.spinChambers.Location = new System.Drawing.Point(342, 218);
+            this.spinChambers.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.spinChambers.Name = "spinChambers";
-            this.spinChambers.Size = new System.Drawing.Size(141, 99);
+            this.spinChambers.Size = new System.Drawing.Size(212, 152);
             this.spinChambers.TabIndex = 2;
             this.spinChambers.Text = "Spin Chamber";
             this.spinChambers.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -189,17 +196,19 @@
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(551, 531);
+            this.panel1.Size = new System.Drawing.Size(826, 817);
             this.panel1.TabIndex = 2;
             // 
             // label11
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(3, 513);
+            this.label11.Location = new System.Drawing.Point(4, 789);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(103, 16);
+            this.label11.Size = new System.Drawing.Size(152, 23);
             this.label11.TabIndex = 6;
             this.label11.Text = "Project By:- Atul";
             // 
@@ -207,9 +216,10 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(13, 23);
+            this.label8.Location = new System.Drawing.Point(20, 35);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(235, 24);
+            this.label8.Size = new System.Drawing.Size(362, 37);
             this.label8.TabIndex = 5;
             this.label8.Text = "Bullet Has Been Fired.";
             // 
@@ -217,9 +227,10 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(13, 22);
+            this.label7.Location = new System.Drawing.Point(20, 34);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(349, 24);
+            this.label7.Size = new System.Drawing.Size(532, 37);
             this.label7.TabIndex = 4;
             this.label7.Text = "Gun Chamber Has Been Spinned.";
             // 
@@ -227,9 +238,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(12, 19);
+            this.label6.Location = new System.Drawing.Point(18, 29);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(489, 28);
+            this.label6.Size = new System.Drawing.Size(743, 42);
             this.label6.TabIndex = 3;
             this.label6.Text = "Bullet Has Been Loaded In Gun Chamber.";
             // 
@@ -237,9 +249,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(48, 191);
+            this.label5.Location = new System.Drawing.Point(72, 294);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(94, 28);
+            this.label5.Size = new System.Drawing.Size(142, 42);
             this.label5.TabIndex = 2;
             this.label5.Text = "Scores";
             // 
@@ -248,36 +261,40 @@
             this.panel3.Controls.Add(this.panel4);
             this.panel3.Controls.Add(this.label9);
             this.panel3.Controls.Add(this.label1);
-            this.panel3.Location = new System.Drawing.Point(0, 222);
+            this.panel3.Location = new System.Drawing.Point(0, 342);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(189, 240);
+            this.panel3.Size = new System.Drawing.Size(284, 369);
             this.panel3.TabIndex = 1;
             // 
             // panel4
             // 
             this.panel4.Controls.Add(this.label10);
             this.panel4.Controls.Add(this.label2);
-            this.panel4.Location = new System.Drawing.Point(91, 0);
+            this.panel4.Location = new System.Drawing.Point(136, 0);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(98, 240);
+            this.panel4.Size = new System.Drawing.Size(147, 369);
             this.panel4.TabIndex = 2;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(12, 49);
+            this.label10.Location = new System.Drawing.Point(18, 75);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(0, 22);
+            this.label10.Size = new System.Drawing.Size(0, 33);
             this.label10.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 12);
+            this.label2.Location = new System.Drawing.Point(34, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 22);
+            this.label2.Size = new System.Drawing.Size(85, 33);
             this.label2.TabIndex = 2;
             this.label2.Text = "Hit!!!";
             // 
@@ -285,40 +302,43 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(20, 49);
+            this.label9.Location = new System.Drawing.Point(30, 75);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(0, 22);
+            this.label9.Size = new System.Drawing.Size(0, 33);
             this.label9.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(21, 12);
+            this.label1.Location = new System.Drawing.Point(32, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 22);
+            this.label1.Size = new System.Drawing.Size(84, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "Save";
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(195, 222);
+            this.pictureBox2.Image = global::game2.Resource1.pictureBox2_Image;
+            this.pictureBox2.Location = new System.Drawing.Point(292, 342);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(353, 240);
+            this.pictureBox2.Size = new System.Drawing.Size(530, 369);
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Coral;
-            this.ClientSize = new System.Drawing.Size(894, 531);
+            this.ClientSize = new System.Drawing.Size(1341, 817);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
             this.ForeColor = System.Drawing.Color.White;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Russian Roulette";
